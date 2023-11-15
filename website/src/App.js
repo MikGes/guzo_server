b7820da8edd7fa743f1351b5c9146f30a21f191f
mikiyas getnet <mikiyasgetnet2@gmail.com>
2023-11-14 16:46:12 -0800

    First commit

---
+++ website/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+    <p className='text-blue-500 font-bold font-xl'>This project is configured with Tailwind and daisy ui</p>
+   </div>
+  );
+}
+
+export default App;
